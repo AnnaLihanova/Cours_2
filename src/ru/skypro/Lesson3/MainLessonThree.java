7acc6d16c563e3752a8008bdde8b44eee1222f7c
Anna Lihanova <anja.lihanova@gmail.com>
2022-02-16 15:31:11 +0300

    Вариант с интерфейсами

--- src/ru/skypro/Lesson3/MainLessonThree.java
+++ src/ru/skypro/Lesson3/MainLessonThree.java
@@ -12,12 +12,14 @@ public class MainLessonThree {
         Bicycle bicycle = new Bicycle("bicycle1",2);
         Bicycle bicycle2 = new Bicycle("bicycle2",2);
 
-        ServiceStation station = new ServiceStation();
+        ServiceStation station = new ServiceStationForTransportWithEngine();
+        ServiceStation station1 = new ServiceStationTransportWithoutEngine();
         station.check(car);
         station.check(car2);
-        station.check(bicycle);
-        station.check(bicycle2);
+        station1.check(bicycle);
+        station1.check(bicycle2);
         station.check(truck);
         station.check(truck2);
+        station1.check(car);
     }
 }
--- src/ru/skypro/Lesson3/ServiceStation.java
+++ src/ru/skypro/Lesson3/ServiceStation.java
@@ -1,34 +1,11 @@
 package ru.skypro.Lesson3;
 
-public class ServiceStation {
+public interface ServiceStation {
 
-    public void check(Car car) {
-        if (car != null) {
-            System.out.println("Обслуживаем " + car.getModelName());
-            for (int i = 0; i < car.getWheelsCount(); i++) {
-                car.updateTyre();
-            }
-            car.checkEngine();
-        }
-    }
+    void check(Car car);
 
-    public void check(Truck truck) {
-        if (truck != null) {
-            System.out.println("Обслуживаем " + truck.getModelName());
-            for (int i = 0; i < truck.getWheelsCount(); i++) {
-                truck.updateTyre();
-            }
-            truck.checkEngine();
-            truck.checkTrailer();
-        }
-    }
+    void check(Bicycle bicycle);
 
-    public void check(Bicycle bicycle) {
-        if (bicycle != null) {
-            System.out.println("Обслуживаем " + bicycle.getModelName());
-            for (int i = 0; i < bicycle.getWheelsCount(); i++) {
-                bicycle.updateTyre();
-            }
-        }
-    }
+    void check(Truck truck);
+
 }
