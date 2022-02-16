4610a03f4d83d9b393dd888d58fd17935e88dfe5
Anna Lihanova <anja.lihanova@gmail.com>
2022-02-16 22:23:45 +0300

    Рефакторинг

--- src/ru/skypro/Lesson3/ServiceStation.java
+++ src/ru/skypro/Lesson3/ServiceStation.java
@@ -2,10 +2,6 @@ package ru.skypro.Lesson3;
 
 public interface ServiceStation {
 
-    void check(Car car);
-
-    void check(Bicycle bicycle);
-
-    void check(Truck truck);
+    void check(Transport transport);
 
 }
